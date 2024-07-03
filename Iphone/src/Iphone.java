7fdb7ac49fa2c81a69b759046c2d20213f9528f0
Mathsx2 <laabc155@gmail.com>
2024-07-03 19:16:53 -0300

    Commit Inicial

---
+++ Iphone/src/Iphone.java
@@ -0,0 +1,69 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    // Métodos do ReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música...");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando a música: " + musica);
+    }
+
+    // Métodos do AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    // Métodos do NavegadorInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba...");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+
+    public static void main(String[] args) {
+        Iphone meuIphone = new Iphone();
+
+        // Testando funcionalidades do ReprodutorMusical
+        meuIphone.tocar();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica("Minha Música Favorita");
+
+        // Testando funcionalidades do AparelhoTelefonico
+        meuIphone.ligar("123-456-7890");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando funcionalidades do NavegadorInternet
+        meuIphone.exibirPagina("www.exemplo.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
---
+++ Iphone/src/IphoneTheus.java
@@ -0,0 +1,6 @@
+public class IphoneTheus {
+	public static void main(String[] args) {
+
+		
+	}
+}
